d56753d3e6424ef17d7998bc6ea095421d9356c2
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2025-09-07 13:22:29 -0400

    Added version-control-workflow project

---
+++ topics/version—— control/version-control-worklow/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    ellipse(mouseX, mouseY, 80, 80);
+}
