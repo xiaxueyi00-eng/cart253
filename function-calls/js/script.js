c071266b95fc5c616071e5e2ed6963acc0a5e3be
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2025-09-09 11:09:41 -0400

    finished simulation of with writer's block

---
+++ function-calls/js/script.js
@@ -0,0 +1,21 @@
+/**
+ * Function Calls
+ * xueyi xia
+ * 
+ * Working on the function calls examples
+*/
+
+"use strict";
+
+
+function setup() {
+    //Once at the beginning of the program
+    createCanvas(640, 480);
+}
+
+function draw() {
+    //Every frame
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
+}
+
