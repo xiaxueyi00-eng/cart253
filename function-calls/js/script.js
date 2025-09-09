8fbf1b07abe148450f3859ce8e49aaa154dd3388
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2025-09-09 11:32:20 -0400

    Added comments to the Blank Page project

--- function-calls/js/script.js
+++ function-calls/js/script.js
@@ -1,21 +1,32 @@
-/**
- * Function Calls
+/*
+ * The Blank Page
  * xueyi xia
  * 
- * Working on the function calls examples
+ * An exploration of the existentia angst of a novelist 
+ * who must sit down at their pink desk and confront the 
+ * abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability
+ * to get started on the project. Try typing! 
 */
 
 "use strict";
 
+/**
+ *  Creates the canvas for our masterpiece
+ */
 
 function setup() {
-    //Once at the beginning of the program
+    // Create the canvas at a standard resolution 
     createCanvas(640, 480);
 }
-
+/**
+* Draws the writer's desktop and a blank plece of paper
+*/
 function draw() {
-    //Every frame
+    //The pink desktop
     background(255, 100, 100);
+    //The plece of paper
     rect(200, 80, 240, 320);
 }
 
