112dab74d5ae7deca5c1748984015777c58d0607
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2025-09-18 13:52:57 -0400

    added the new world

---
+++ variables—challenge/js/script.js
@@ -0,0 +1,58 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+
+/**
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
+ */
+
+"use strict";
+
+// Our friend Mr. Furious
+let mrFurious = {
+    // Position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // Colour
+    fill: {
+        r: 255,
+        g: 225,
+        b: 225
+    }
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
+function draw() {
+    background(160, 180, 200);
+
+    // Draw Mr. Furious as a coloured circle
+    push();
+    noStroke();
+    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
+
+}
