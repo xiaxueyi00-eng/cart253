6db855c1c984ec7a247f9f7f5d1d9080ee0fc16c
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2025-09-23 09:01:49 -0400

    changed

--- variables—challenge/js/script.js
+++ variables—challenge/js/script.js
@@ -1,13 +1,12 @@
+
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
-
 
 // Our friend Mr. Furious
 let mrFurious = {
@@ -18,8 +17,8 @@ let mrFurious = {
     // Colour
     fill: {
         r: 255,
-        g: 225,
-        b: 225
+        g: 0,
+        b: 0
     }
 };
 
@@ -28,13 +27,15 @@ let mrFurious = {
  */
 function setup() {
     createCanvas(400, 400);
-}
+};
+
+
 
 /**
  * Draw (and update) Mr. Furious
  */
 function draw() {
-    background(160, 180, 200);
+    background(0, 0, 0);
 
     // Draw Mr. Furious as a coloured circle
     push();
@@ -42,5 +43,28 @@ function draw() {
     fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
     ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
     pop();
-
 }
+
+function setup() {
+    // Create the canvas
+    createCanvas(640, 640);
+}
+
+function draw() {
+
+    circle(50, 50, 25);
+
+    // Draw a circle at the mouse coordinates
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    // We use the variable names mouseX and mouseY instead
+    // of numbers for the x and y coordinates of our circle
+    // JavaScript will *use the values inside them* (the numbers)
+    // to send as the x and y arguments of ellipse()
+    // And that will mean the ellipse will be drawn with its (x, y)
+    // position set to the current mouse (x, y) position.
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
+}
+
